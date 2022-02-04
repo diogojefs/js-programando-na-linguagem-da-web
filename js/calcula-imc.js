5bd7944fcc9f2b18c96b413d1eff74ca84168ee5
diogojefs <diogojefs@gmail.com>
2022-02-03 22:50:19 -0300

    Projeto completo

---
+++ js/calcula-imc.js
@@ -0,0 +1,61 @@
+console.log("Fui carregado de um arquivo externo");
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
+
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
+
+    if (!pesoEhValido) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (!alturaEhValida) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
+    } else if (!alturaEhValida) {
+        tdImc.textContent = "Altura inválida!"
+    } else tdImc.textContent = "Peso inválido!"
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+    if (peso > 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function validaAltura(altura) {
+    if (altura > 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
